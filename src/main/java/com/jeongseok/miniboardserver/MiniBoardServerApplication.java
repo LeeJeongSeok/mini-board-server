52fdc24b6b4cd8cd39c448b825e216741823db4c
JeongSeok <ljs0429777@gmail.com>
2024-05-16 00:59:16 +0900

    :tada: 프로젝트 시작

---
+++ src/main/java/com/jeongseok/miniboardserver/MiniBoardServerApplication.java
@@ -0,0 +1,13 @@
+package com.jeongseok.miniboardserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBoardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniBoardServerApplication.class, args);
+	}
+
+}
