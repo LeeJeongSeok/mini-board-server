485410e087c8656bf53158b9372970d4e12b0216
JeongSeok <ljs0429777@gmail.com>
2024-05-17 20:13:55 +0900

    :sparkles: 게시글 생성 기능 추가

--- src/main/java/com/jeongseok/miniboardserver/controller/PostController.java
+++ src/main/java/com/jeongseok/miniboardserver/controller/PostController.java
@@ -1,8 +1,48 @@
 package com.jeongseok.miniboardserver.controller;
 
-import org.springframework.stereotype.Controller;
+import com.jeongseok.miniboardserver.dto.post.PostRequestDto;
+import com.jeongseok.miniboardserver.service.PostService;
+import jakarta.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/api/posts")
+@RequiredArgsConstructor
 public class PostController {
 
+	private final PostService postService;
+
+	@PostMapping("")
+	public ResponseEntity<String> createPost(@Valid @RequestBody PostRequestDto postRequestDto) {
+
+		// 실제 post 처리 로직
+		postService.save(postRequestDto);
+
+		return new ResponseEntity<>("Post created successfully", HttpStatus.CREATED);
+	}
+
+	// 유효성 검사 실패 시 발생하는 예외를 처리하는 메서드
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+		Map<String, String> errors = new HashMap<>();
+		ex.getBindingResult().getAllErrors().forEach(error -> {
+			String fieldName = ((FieldError) error).getField();
+			String errorMessage = error.getDefaultMessage();
+			errors.put(fieldName, errorMessage);
+		});
+		return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+	}
 }
--- src/main/java/com/jeongseok/miniboardserver/repository/PostRepository.java
+++ src/main/java/com/jeongseok/miniboardserver/repository/PostRepository.java
@@ -1,8 +1,10 @@
 package com.jeongseok.miniboardserver.repository;
 
+import com.jeongseok.miniboardserver.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostRepository {
+public interface PostRepository extends JpaRepository<Post, Long> {
 
 }
