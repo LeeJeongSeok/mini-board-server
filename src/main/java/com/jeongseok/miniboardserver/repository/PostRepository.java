069b7262357ca8daa83f876707d63b9b53d85fdd
JeongSeok <ljs0429777@gmail.com>
2024-05-22 00:48:07 +0900

    :sparkles: 전체 게시글 조회시 use_yn이 y인 게시글만 조회하는 기능 추가

--- src/main/java/com/jeongseok/miniboardserver/repository/PostRepository.java
+++ src/main/java/com/jeongseok/miniboardserver/repository/PostRepository.java
@@ -1,10 +1,15 @@
 package com.jeongseok.miniboardserver.repository;
 
 import com.jeongseok.miniboardserver.domain.Post;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+	// useYn이 'Y'인 게시글만 조회하는 메서드
+	@Query("SELECT p FROM Post p WHERE p.useYn = 'Y'")
+	List<Post> findAllByUseYn();
 }
