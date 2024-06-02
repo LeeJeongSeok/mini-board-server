8a4ea04301449dc4d3a8f9262c4c13fe302f1865
JeongSeok <ljs0429777@gmail.com>
2024-06-02 12:45:44 +0900

    테스트 추가

---
+++ src/test/java/com/jeongseok/miniboardserver/service/PostServiceTest.java
@@ -0,0 +1,10 @@
+package com.jeongseok.miniboardserver.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceTest {
+
+}
