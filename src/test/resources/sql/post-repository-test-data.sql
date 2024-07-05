db057f961469bbe0ecb9def0cb70dbe28bf85325
JeongSeok <ljs0429777@gmail.com>
2024-07-06 01:11:50 +0900

    test: 테스트 데이터를 위한 sql 파일 추가

---
+++ src/test/resources/sql/post-repository-test-data.sql
@@ -0,0 +1,8 @@
+INSERT INTO post
+    (title, content, author, created_at, updated_at, use_yn)
+VALUES
+    ('Title 1', 'Content 1', 'Author 1', NOW(), NOW(), 'Y'),
+    ('Title 2', 'Content 2', 'Author 2', NOW(), NOW(), 'Y'),
+    ('Title 3', 'Content 3', 'Author 3', NOW(), NOW(), 'Y'),
+    ('Title 4', 'Content 4', 'Author 4', NOW(), NOW(), 'N'),
+    ('Title 5', 'Content 5', 'Author 5', NOW(), NOW(), 'N');
